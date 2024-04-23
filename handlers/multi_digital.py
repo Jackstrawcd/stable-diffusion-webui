c703c35a14a8d62e7b714cb19659ef3cd2bfbe4b
Jackstrawcd <296601946@qq.com>
2024-04-23 17:03:50 +0800

    [feat](*): 修改HOOK snapshot_download

--- handlers/multi_digital.py
+++ handlers/multi_digital.py
@@ -13,74 +13,19 @@ import shutil
 import dlib
 import cv2
 import numpy as np
-from transformers import PreTrainedTokenizerBase, PreTrainedModel
 from PIL import ImageOps, Image
 from handlers.txt2img import Txt2ImgTask, Txt2ImgTaskHandler
 from worker.task import TaskType, TaskProgress, Task, TaskStatus
 from modules.processing import process_images
 from handlers.utils import load_sd_model_weights, save_processed_images, ADetailer, mk_tmp_dir
 from handlers.utils import get_tmp_local_path
-from sd_scripts.library.transformers_pretrained import ori_tokenizer_from_pretrained, ori_model_from_pretrained
+from transformers import PreTrainedModel, PreTrainedTokenizerBase
 from modelscope.outputs import OutputKeys
 from modelscope.pipelines import pipeline
 from modelscope.utils.constant import Tasks
-from modelscope import snapshot_download
 
 
-def patch_tokenizer_base():
-    """ Monkey patch PreTrainedTokenizerBase.from_pretrained to adapt to modelscope hub.
-    """
-    ori_from_pretrained = ori_tokenizer_from_pretrained
 
-    @classmethod
-    def from_pretrained(cls, pretrained_model_name_or_path, *model_args,
-                        **kwargs):
-        ignore_file_pattern = [r'\w+\.bin', r'\w+\.safetensors']
-        if "use_modelscope" in kwargs:
-            if not os.path.exists(pretrained_model_name_or_path):
-                revision = kwargs.pop('revision', None)
-                model_dir = snapshot_download(
-                    pretrained_model_name_or_path,
-                    revision=revision,
-                    ignore_file_pattern=ignore_file_pattern)
-            else:
-                model_dir = pretrained_model_name_or_path
-            return ori_from_pretrained(cls, model_dir, *model_args, **kwargs)
-        else:
-            model_dir = pretrained_model_name_or_path
-            return ori_from_pretrained(cls, model_dir, *model_args, **kwargs)
-
-    PreTrainedTokenizerBase.from_pretrained = from_pretrained
-
-
-def patch_model_base():
-    """ Monkey patch PreTrainedModel.from_pretrained to adapt to modelscope hub.
-    """
-    ori_from_pretrained = ori_model_from_pretrained
-
-    @classmethod
-    def from_pretrained(cls, pretrained_model_name_or_path, *model_args,
-                        **kwargs):
-        ignore_file_pattern = [r'\w+\.safetensors']
-        if "use_modelscope" in kwargs:
-            if not os.path.exists(pretrained_model_name_or_path):
-                revision = kwargs.pop('revision', None)
-                model_dir = snapshot_download(
-                    pretrained_model_name_or_path,
-                    revision=revision,
-                    ignore_file_pattern=ignore_file_pattern)
-            else:
-                model_dir = pretrained_model_name_or_path
-            return ori_from_pretrained(cls, model_dir, *model_args, **kwargs)
-        else:
-            model_dir = pretrained_model_name_or_path
-            return ori_from_pretrained(cls, model_dir, *model_args, **kwargs)
-
-    PreTrainedModel.from_pretrained = from_pretrained
-
-
-patch_tokenizer_base()
-patch_model_base()
 
 
 class MultiGenDigitalPhotoLoraMeta:
